b8bc8ae226494e8dd5f8dafb089eba99476d6630
wangbing01 <bing.wang@gwm.cn>
2021-04-21 18:27:40 +0800

    城市大厅Activity

---
+++ app/src/main/java/com/cyberalaer/hybrid/ui/city/MyCityActivity.java
@@ -0,0 +1,21 @@
+package com.cyberalaer.hybrid.ui.city;
+
+import com.cyberalaer.hybrid.R;
+import com.cyberalaer.hybrid.base.BaseViewBindActivity;
+import com.cyberalaer.hybrid.databinding.ActivityWelcomBinding;
+
+/**
+ * 我的城市/城市大厅
+ */
+public class MyCityActivity extends BaseViewBindActivity<ActivityWelcomBinding> {
+
+    @Override
+    public void onViewCreated() {
+    }
+
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_my_city;
+    }
+
+}
