02a1778167d06cdaee255bff5c4c950fec9c2cec
HJW <499655607@qq.com>
2021-11-07 15:47:24 +0800

    优化分享记录列表；列表类型Icon

--- app/src/main/java/llc/metaversenetwork/app/data/BillUtil.java
+++ app/src/main/java/llc/metaversenetwork/app/data/BillUtil.java
@@ -1,6 +1,7 @@
 package llc.metaversenetwork.app.data;
 
 import com.alaer.lib.api.bean.ActiveBill;
+
 import llc.metaversenetwork.app.R;
 
 public class BillUtil {
@@ -12,13 +13,15 @@ public class BillUtil {
 
     // 1: 兑换 / 2: 到期 / 13: 统计 / 101: 分享
     public int parseTypeIcon(int billType) {
-        if (billType == 1)
+        if (billType == 1 || billType == 102 || billType == 103 || billType == 3)
             return R.drawable.ic_type_exchange;
-        if (billType == 2)
+        if (billType == 2 || billType == 9 || billType == 8 || billType == 104)
             return R.drawable.ic_type_expire;
-        if (billType == 13)
+        if (billType == 11 || billType == 12 || billType == 6)
+            return R.drawable.ic_type_fire;
+        if (billType == 13 || billType == 12 || billType == 14)
             return R.drawable.ic_type_statistics;
-        if (billType == 101)
+        if (billType == 101 || billType == 5 || billType == 10)
             return R.drawable.ic_type_share;
 
         return R.drawable.ic_type_statistics;
